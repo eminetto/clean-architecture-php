aa833575ce5e073f6dfdcd6a69d0677f6fbcfe84
Elton Minetto <eminetto@gmail.com>
2018-05-23 22:04:12 -0300

    Cli interface

---
+++ cli/search.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/autoload.php';
+$container = require 'config/container.php';
+$repo = $container->get(Bookmark\UseCase\SqliteRepository::class);
+$service = new Bookmark\UseCase\Service($repo);
+$result = $service->search($argv[1]);
+foreach ($result as $key => $value) {
+    printf("ID: %s Name: %s URL: %s \n", $value->id, $value->name, $value->link);
+}
--- config/routes.php
+++ config/routes.php
@@ -33,8 +33,7 @@ use Zend\Expressive\MiddlewareFactory;
  * );
  */
 return function (Application $app, MiddlewareFactory $factory, ContainerInterface $container) : void {
-    $app->get('/', App\Handler\HomePageHandler::class, 'home');
-    $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
+    $app->get('/v1/ping', App\Handler\PingHandler::class, 'api.ping');
     $app->get('/v1/bookmark', Bookmark\Handler\IndexHandler::class, 'bookmark.index');
     $app->post('/v1/bookmark', Bookmark\Handler\PostHandler::class, 'bookmark.post');
 };
